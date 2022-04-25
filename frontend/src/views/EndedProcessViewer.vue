0b00ed59f71fa0af037746f370d7335bf004e012
ivano <ivano.dalmasso@gmail.com>
2022-04-25 18:43:02 +0200

    little fixes

--- frontend/src/views/EndedProcessViewer.vue
+++ frontend/src/views/EndedProcessViewer.vue
@@ -2,9 +2,16 @@
   <div class="ended-process-viewer">
     <h2>Processo {{ processName }}</h2>
     <vue-three-sixty
+      v-if="savewebp"
       :amount="numImages"
       :imagePath="imageDir"
       fileName="{index}.webp"
+    />
+    <vue-three-sixty
+      v-else
+      :amount="numImages"
+      :imagePath="imageDir"
+      fileName="{index}.jpg"
     />
   </div>
 </template>
@@ -16,11 +23,25 @@ export default {
     processName: String,
     numImages: Number,
   },
+  data() {
+    return {
+      savewebp: false,
+    }
+  },
   computed: {
     imageDir() {
       //console.log(this.processName)
       return '/process-images/' + this.processName + '/'
     },
+  },
+  mounted() {
+    fetch('/api/configuration')
+      .then((data) => {
+        return data.json()
+      })
+      .then((config) => {
+        this.savewebp = config.server.saveAsWebP
+      })
   },
 }
 </script>
